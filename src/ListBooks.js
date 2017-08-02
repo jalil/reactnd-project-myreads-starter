4a030eed2981985a8d68f487705e8e1698637ead
Your Name <you@example.com>
2017-08-01 19:37:56 -0700

    Added ListBook component

---
+++ src/ListBooks.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import Book from './Book'
+
+export class ListBooks extends Component {
+
+  render() {
+
+    let current = this.props.books.filter((book) => book.shelf === 'currentlyReading')
+
+    let want    = this.props.books.filter((book) => book.shelf === 'wantToRead')
+
+    let read    = this.props.books.filter((book) => book.shelf === 'read')
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {current.map((book) => (
+                    <li key={book.id}>
+                      <Book book={book} Move={Move} />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {want.map((book) => (
+                    <li key={book.id}>
+                      <Book book={book} Move={Move} />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {read.map((book) => (
+                    <li key={book.id}>
+                      <Book book={book} Move={Move} />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
